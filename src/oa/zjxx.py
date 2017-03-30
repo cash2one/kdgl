7c16adeaa8a62e990c533c520fb62cbf84209046
“chengdegong” <chengdegong@weizoom.com>
2017-03-30 16:37:36 +0800

    提交代码

--- src/oa/zjxx.py
+++ src/oa/zjxx.py
@@ -6,8 +6,13 @@ from django.template import RequestContext
 from django.template.loader import render_to_string
 import os, json, datetime, xlsxwriter
 from zjyw_utils import *
+from const import *
 
 log.init_log( 'kdgl' , True )
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 def kd_info_view(request):
     try:
@@ -26,15 +31,9 @@ def kd_info_view(request):
             while rs.next():
                 rs_lst = rs.to_dict()
                 ls.append(rs_lst)
-        if ls:
-            content['xym'] = '000000'
-            content['cont'] = render_to_string( 'kdxx_idx.html', {'kd_info':ls} )
-            return HttpResponse( json.dumps( content), content_type='application/json' )
-        else:
-            # 返回错误信息
-            log.info( "kdgl", '快递信息不存在')
-            content['xyxx'] = '快递信息不存在！'
-            return HttpResponse( json.dumps( content) , content_type='application/json' )
+        content['xym'] = '000000'
+        content['cont'] = render_to_string( 'kdxx_idx.html', {'kd_info':ls} )
+        return HttpResponse( json.dumps( content), content_type='application/json' )
     except Exception, e :
         content['xyxx'] = '后台函数[kd_info_view]执行错误:%s' % str( e )
         log.exception( 'kdgl', '后台函数[kd_info_view]执行错误:%s', str( e ) )
@@ -123,15 +122,49 @@ def kd_edit_view(request):
         shelf_number = request.POST.get('shelf_number','').encode('utf8')
         type = request.POST.get('type','').encode('utf8')
         created_at = request.POST.get('created_at','').encode('utf8')
+        #校验快递员编号是否重复
+        sql = "select number from auth_user where number='%s'"%express_user_id
+        with myapi.connection() as con:
+            cur = con.cursor()
+            rs = myapi.sql_execute(cur, sql)
+            if not rs.next():
+                log.info( "kdgl", '快递员编号不存在')
+                content['xyxx'] = '快递员编号不存在'
+                return HttpResponse( json.dumps( content) , content_type='application/json' )
+        sql2 = "select type from express_info where id='%s'"%id
+        with myapi.connection() as con:
+            cur = con.cursor()
+            rs = myapi.sql_execute(cur, sql2)
+            if rs.next():
+                rs_dic = rs.to_dict()
+                type_old = rs_dic['type']
         # 修改快递信息
         sql = "update express_info set express_classification='%s', express_user_id='%s', sender_name='%s', express_money='%s', express_number='%s', express_status='%s', sender_address='%s', reciver_name='%s', sender_phone='%s', reciver_address='%s', shelf_number='%s', reciver_phone='%s', type='%s', created_at='%s', type='%s' where id='%s'" % (express_classification,  express_user_id,  sender_name, express_money,  express_number,  express_status, sender_address, reciver_name, sender_phone, reciver_address, shelf_number,  reciver_phone,  type, created_at, type, id )
         with myapi.connection() as con:
             cur = con.cursor()
             cur.execute(sql)
-           
         log.info( "kdgl", "修改快递信息sql【%s】", sql )
         if cur.rowcount == 1:
             log.info( "kdgl", "修改快递信息成功！" )
+            # 计算用户绩效
+            sql2 = "select performance from auth_user where number='%s'"%express_user_id
+            with myapi.connection() as con:
+                cur = con.cursor()
+                rs = myapi.sql_execute(cur, sql2)
+                if rs.next():
+                    rs_dic = rs.to_dict()
+                    performance = rs_dic['performance']
+            if type=='2' and type_old=='1':
+                performance = str(float(performance)-float(express_money)*0.1)
+            elif type=='1' and type_old=='2':
+                performance = str(float(performance)+float(express_money)*0.1)
+            else:
+                performance = performance
+            # 更新用户绩效
+            sql3 = "update auth_user set performance='%s' where number='%s'" % ( performance, express_user_id )
+            with myapi.connection() as con:
+                cur = con.cursor()
+                cur.execute(sql3)
             content['xym'] = '000000'
             content['xyxx'] = '修改快递信息成功！'
             return HttpResponse( json.dumps( content), content_type='application/json' )
@@ -189,35 +222,43 @@ def add_kdxx_view(request):
                 log.info( "kdgl", '订单编号已存在')
                 content['xyxx'] = '订单编号已存在'
                 return HttpResponse( json.dumps( content) , content_type='application/json' )
-        # 新增快递信息
-        sql = "insert into  express_info(type, shelf_number, express_status, express_number, express_money, reciver_phone, reciver_address, reciver_name, sender_phone, sender_address, sender_name,express_user_id, express_classification,created_at)values( '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')" % ( type, shelf_number, express_status, express_number, express_money, reciver_phone, reciver_address, reciver_name, sender_phone, sender_address, sender_name,express_user_id, express_classification,created_at )
+        #校验快递员编号是否重复
+        sql = "select number from auth_user where number='%s'"%express_user_id
         with myapi.connection() as con:
             cur = con.cursor()
-            cur.execute(sql)
-        log.info( "kdgl", "添加快递信息sql【%s】", sql )
+            rs = myapi.sql_execute(cur, sql)
+            if not rs.next():
+                log.info( "kdgl", '快递员编号不存在')
+                content['xyxx'] = '快递员编号不存在'
+                return HttpResponse( json.dumps( content) , content_type='application/json' )
+        # 新增快递信息
+        sql1 = "insert into  express_info(type, shelf_number, express_status, express_number, express_money, reciver_phone, reciver_address, reciver_name, sender_phone, sender_address, sender_name,express_user_id, express_classification,created_at)values( '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')" % ( type, shelf_number, express_status, express_number, express_money, reciver_phone, reciver_address, reciver_name, sender_phone, sender_address, sender_name,express_user_id, express_classification,created_at )
+        with myapi.connection() as con:
+            cur = con.cursor()
+            cur.execute(sql1)
+        log.info( "kdgl", "添加快递信息sql【%s】", sql1 )
         if cur.rowcount == 1:
             log.info( "add_kdxx", "添加快递信息成功！" )
             # 计算用户绩效
-            sql = "select performance from auth_user where number='%s'"%express_user_id
+            sql2 = "select performance from auth_user where number='%s'"%express_user_id
             with myapi.connection() as con:
                 cur = con.cursor()
-                rs = myapi.sql_execute(cur, sql)
+                rs = myapi.sql_execute(cur, sql2)
                 if rs.next():
                     rs_dic = rs.to_dict()
                     performance = rs_dic['performance']
-            performance = str(float(performance)+float(express_money)*0.1)
+            performance = str(float(performance)+float(express_money)*0.1) if type=='1' else performance
             # 更新用户绩效
-            sql = "update auth_user set performance='%s' where number='%s'" % ( performance, express_user_id )
+            sql3 = "update auth_user set performance='%s' where number='%s'" % ( performance, express_user_id )
             with myapi.connection() as con:
                 cur = con.cursor()
-                cur.execute(sql)
+                cur.execute(sql3)
             content['xym'] = '000000'
             content['xyxx'] = '添加快递信息成功！'
             return HttpResponse( json.dumps( content), content_type='application/json' )
         else:
             # 返回错误信息
             log.info( "kdgl", '添加快递信息失败')
-            
             content['xyxx'] = '添加快递信息失败！'
             return HttpResponse( json.dumps( content) , content_type='application/json' )
     except Exception, e :
@@ -421,25 +462,22 @@ def file_download_tz_view(request):
         log.exception( 'kdgl', '后台函数[file_download_tz_view]执行错误:%s', str( e ) )
         return HttpResponse( json.dumps( content) , content_type='application/json' )
 
-def file_iterator(file_name, chunk_size=512):
-        with open(file_name) as f:
+def file_iterator(file_name):
+        with open(file_name, 'rb') as f:
             while True:
-                c = f.read(chunk_size)
+                c = f.read()
                 if c:
                     yield c
                 else:
                     break
 def download_view(request):
-    import sys
-    reload(sys)
-    sys.setdefaultencoding('utf-8')
     try:
         content={}
         ls=[]
         content['xym'] = '0'
         if not request.session.get('username',''):
             return HttpResponseRedirect('/')
-        title = [u'快递单号', u'快递类型', u'快递分类', u'快递费' , u'快递员编号', u'发货人姓名',
+        title = [u'快递单号', u'快递类型', u'快递分类', u'快递费(元)' , u'快递员编号', u'发货人姓名',
          u'发货人联系方式', u'发货人地址', u'收货人姓名', u'收货人联系方式', u'收货人地址', u'货架编号',
          u'快递状态', u'创建时间']
         sql = "select * from express_info order by id desc"
@@ -462,7 +500,8 @@ def download_view(request):
             print '-------%s'%repr(l)
             tmp = [
                 l['express_number'],
-                l['type'],
+                u'发送' if l['type']=='1' else u'接收' ,
+                classification_dic.get(l['express_classification'],'--'),
                 l['express_money'],
                 l['express_user_id'],
                 l['sender_name'],
@@ -472,24 +511,22 @@ def download_view(request):
                 l['reciver_phone'],
                 l['reciver_address'],
                 l['shelf_number'],
-                l['express_status'],
+                status_dic.get(l['express_status'],'--'),
                 l['created_at']
             ]
             table.write_row("A{}".format(tmp_line), tmp)
             tmp_line += 1
         workbook.close()
+        # response = HttpResponse()
+        # response['Content-Disposition'] = "attachment;filename='{0}'".format(filename)
+        # content = open(file_path, 'rb').read()
+        # response.write(content)
+        # return response
+
         from django.http import StreamingHttpResponse
         response = StreamingHttpResponse(file_iterator(file_path))
         response['Content-Type'] = 'application/octet-stream'
-        response['Content-Disposition'] = 'attachment;filename="{0}"'.format(filename)
-        return response
-
-        # from django.core.servers.basehttp import FileWrapper
-        # wrapper = FileWrapper(file(file_path))
-        # response = HttpResponse(wrapper, content_type='text/plain')
-        # response['Content-Length'] = os.path.getsize(file_path)
-        # response['Content-Encoding'] = 'utf-8'
-        # response['Content-Disposition'] = 'attachment;filename=%s' % filename
+        response['Content-Disposition'] = "attachment;filename='{0}'".format(filename)
         return response
     except Exception, e :
         content['xyxx'] = '后台函数[download_view]执行错误:%s' % str( e )
